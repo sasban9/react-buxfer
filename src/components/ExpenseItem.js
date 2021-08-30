a6702f6293e838c81a810a382d426e58a467ec14
sasban <azure.saswata@gmail.com>
2021-08-30 13:00:37 +0530

    Dynamic data and Expressions in JSX.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 11029;
+
   return (
     <div className="expense-item">
-      <div>28 Mar 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">₹ 11029</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">₹ {expenseAmount}</div>
       </div>
     </div>
   );
