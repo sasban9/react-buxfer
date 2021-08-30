e7932111ebe17b1b623f8a9197afa649c776e733
sasban <azure.saswata@gmail.com>
2021-08-30 16:36:21 +0530

    How component functions are executed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,21 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  let title = props.title;
+
+  const clickHandler = () => {
+    title = "Changed!";
+    console.log("Updated !!!");
+  };
+
   return (
     <Card className="expense-item" data-index={props.index}>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">₹ {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
