86fb40d5fc1f819dd4823afd7e58730826642449
sasban <azure.saswata@gmail.com>
2021-08-31 20:13:34 +0530

    Adding a reusable Button Component.

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,22 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
 const AddUser = (props) => {
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    };
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" />
-      <label htmlFor="age">Age (years)</label>
-      <input type="number" id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (years)</label>
+        <input type="number" id="age" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
